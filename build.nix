899e21ddfb528549d37616f44bc10214a5bbeb22
Kirill Kuznetsov <kirill.desirehd@gmail.com>
2021-08-02 16:09:09 +0300

    Optimize getting index state

--- build.nix
+++ build.nix
@@ -8,9 +8,33 @@ let
     };
   in
     import sources.nixpkgs-unstable args;
+
+  update-index-state-hashes = indexStateHashesPath: with pkgs; with lib; writeShellScriptBin "update-index-state-hashes" ''
+    export PATH="${makeBinPath [ coreutils pkgs.haskell-nix.nix-tools.ghc8105 nix curl ]}"
+
+    start=${let ls = attrNames (import indexStateHashesPath); in elemAt ls (length ls - 1)}
+
+    cat ${indexStateHashesPath} | head -n -1
+
+    curl "https://hackage.haskell.org/01-index.tar.gz" --output index.tar.gz --fail-early
+
+    for d in $(seq -f '%.f' $(date -u +%s -d $start) 86400 $(date -u +%s)) ; do
+      dt=$(date -u +%Y-%m-%d -d @$d)
+
+      if [[ "''${dt}T00:00:00Z" != "$start" ]]; then
+        truncate-index -o ''${dt}-01-index.tar.gz -i index.tar.gz -s "''${dt}T00:00:00Z"
+        sha256=$(nix-hash --flat --type sha256 ''${dt}-01-index.tar.gz)
+        echo "  \"''${dt}T00:00:00Z\" = \"''${sha256}\";"
+      fi
+    done
+
+    echo '}'
+  '';
+
+  update-index = update-index-state-hashes ./index-state-hashes.nix;
 in
 {
-  inherit pkgs;
+  inherit pkgs update-index;
 
   update-hackage = with pkgs; with lib; writeScript "update-hackage-nix.sh" ''
     #!${stdenv.shell}
@@ -26,6 +50,6 @@ in
     hackage-to-nix .
 
     echo "Running update-index-state-hashes..."
-    ${pkgs.haskell-nix.update-index-state-hashes}/bin/update-index-state-hashes > index-state-hashes.nix
+    ${update-index}/bin/update-index-state-hashes > index-state-hashes.nix
   '';
 }
